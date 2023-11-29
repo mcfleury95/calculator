4f42c1a4eae8cd347f10fc14311c07f73c8d6359
mcfleury95 <hansbarre95@gmail.com>
2023-11-29 11:34:45 +0100

    calculatrice.py

--- calculatrice.py
+++ calculatrice.py
@@ -1,46 +1,38 @@
-def addition(a, b):
-    return a + b
+def calculatrice():
+    while True:
+        print("Sélectionnez une opération:")
+        print("1. Addition")
+        print("2. Soustraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Quitter")
 
-def soustraction(a, b):
-    return a - b
+        choix = input("Entrez le numéro de l'opération souhaitée (1/2/3/4/5): ")
 
-def multiplication(a, b):
-    return a * b
+        if choix == '5':
+            print("Au revoir !")
+            break
 
-def division(a, b):
-    if b != 0:
-        return a / b
-    else:
-        return "Erreur : division par zéro"
+        a = float(input("Entrez le premier nombre: "))
+        b = float(input("Entrez le deuxième nombre: "))
 
-while True:
-    print("Sélectionnez une opération:")
-    print("1. Addition")
-    print("2. Soustraction")
-    print("3. Multiplication")
-    print("4. Division")
-    print("5. Quitter")
+        if choix == '1':
+            resultat = a + b
+        elif choix == '2':
+            resultat = a - b
+        elif choix == '3':
+            resultat = a * b
+        elif choix == '4':
+            if b != 0:
+                resultat = a / b
+            else:
+                print("Erreur : division par zéro")
+                continue
+        else:
+            print("Choix invalide. Veuillez entrer un numéro valide (1/2/3/4/5).")
+            continue
 
-    choix = input("Entrez le numéro de l'opération souhaitée (1/2/3/4/5): ")
+        print("Le résultat de l'opération est:", resultat)
 
-    if choix == '5':
-        print("Au revoir !")
-        break
-
-    a = float(input("Entrez le premier nombre: "))
-    b = float(input("Entrez le deuxième nombre: "))
-
-    if choix == '1':
-        resultat = addition(a, b)
-    elif choix == '2':
-        resultat = soustraction(a, b)
-    elif choix == '3':
-        resultat = multiplication(a, b)
-    elif choix == '4':
-        resultat = division(a, b)
-    else:
-        print("Choix invalide. Veuillez entrer un numéro valide (1/2/3/4/5).")
-        continue
-
-    print("Le résultat de l'opération est:", resultat)
-
+# Appel de la fonction de la calculatrice
+calculatrice()
--- test_calculatrice.py
+++ test_calculatrice.py
@@ -1,23 +1,13 @@
-def calculatrice():
+def test_calculatrice():
     assert addition(3, 5) == 8
-    assert addition(-1, 1) == 0
-    assert addition(0, 0) == 0
-
     assert soustraction(8, 3) == 5
-    assert soustraction(-1, 1) == -2
-    assert soustraction(0, 0) == 0
-
     assert multiplication(3, 5) == 15
-    assert multiplication(-1, 1) == -1
-    assert multiplication(0, 0) == 0
 
+    # Test de division
     assert division(8, 2) == 4
-    assert division(-6, 3) == -2
-    assert division(0, 5) == 0
-
     assert division(5, 0) == "Erreur : division par zéro"
 
     print("Tous les tests ont réussi !")
 
 # Appel de la fonction de test
-calculatrice()
+test_calculatrice()
